3eff886acb09510851296de91115e6e8a95958be
rizal-mohamad <rizalmohamad209@gmail.com>
2021-03-01 18:44:11 +0700

    Add Procfile

--- config/db.config.js
+++ config/db.config.js
@@ -2,10 +2,14 @@ const mysql = require("mysql");
 const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const conn = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
+  //   host: DB_HOST,
+  //   user: DB_USERNAME,
+  //   password: DB_PASSWORD,
+  //   database: DB_DATABASE,
+  host: "remotemysql.com",
+  user: "jh0ceKOCmG",
+  password: "ABziUsLgyw",
+  database: "jh0ceKOCmG",
   multipleStatements: true,
 });
 
